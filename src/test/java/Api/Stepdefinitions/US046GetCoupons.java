646d56552d055c0be955249da7e2ed4937410d79
lazKorsan <ahmeteldes2002@gmail.com>
2026-01-01 01:46:33 +0300

    debug

--- src/test/java/Api/Stepdefinitions/US046GetCoupons.java
+++ src/test/java/Api/Stepdefinitions/US046GetCoupons.java
@@ -1,6 +1,6 @@
 package Api.Stepdefinitions;
 
-import Api.TestNg.Courses.CoursesMethod;
+
 import io.cucumber.java.en.Given;
 
 public class US046GetCoupons {
@@ -9,7 +9,7 @@ public class US046GetCoupons {
     @Given("ASD")
     public void asd() {
 
-        CoursesMethod.GetCourses_TC01();
+
     }
 
 
