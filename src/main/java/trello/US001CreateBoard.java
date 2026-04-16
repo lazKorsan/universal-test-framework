fcdf7f0f3b1b23d633c7689e4575bbcc5def0c51
lazKorsan <ahmeteldes2002@gmail.com>
2026-04-17 00:04:16 +0300

    method guncellemesi

--- src/main/java/trello/US001CreateBoard.java
+++ src/main/java/trello/US001CreateBoard.java
@@ -8,8 +8,10 @@ import static io.restassured.RestAssured.given;
 
 public class US001CreateBoard {
 
-    String apiKey=ConfigLoader.getProp("trelloApiKey");
-    String token=ConfigLoader.getProp("trelloToken");
+   // String apiKey=ConfigLoader.getProp("trelloApiKey");
+   // String token=ConfigLoader.getProp("trelloToken");
+    String apiKey="06bac02cfc9639df5514c36f580ecb8b";
+    String token="ATTAd7762c52809ed4d798bd5c1cbd078e4fa6c1834e2c16b87f34d061bdae488c1eD619329A";
 
     @Test
     public void createBoard(){
@@ -35,10 +37,18 @@ public class US001CreateBoard {
         response.prettyPrint();
         response.then().statusCode(200);
 
-        String id = "69dc00ae23bb19b37c7cc7eb";
-        String visibleIdURL="https://trello.com/b/zdNDW9US/newworkspace";
-        String secretId="zdNDW9US";
-        String shortUrl ="https://trello.com/b/zdNDW9US";
+        // "id": "69dc26accd366993583e9982",
+        //    "name": "newWorkSpace307",
+        //    "desc": "",
+        //    "descData": null,
+        //    "closed": false,
+        //    "idOrganization": "69dbd8d23e23dca76dda8cde",
+        //    "idEnterprise": null,
+        //    "pinned": false,
+        //    "url": "https://trello.com/b/lOYOD5eK/newworkspace307",
+        //    "shortUrl": "https://trello.com/b/lOYOD5eK",
+
+
 
 
     }
